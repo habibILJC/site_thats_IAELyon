74c534bc2f4db50fa12870c9049a15124a97191e
Mohamed Sanogo <mohamed.sanogo@etu.univ-lyon1.fr>
2026-06-16 09:40:14 +0200

    ajout du site

---
+++ app.js
@@ -0,0 +1,20 @@
+// Menu mobile
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+if (burger) {
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+  nav.querySelectorAll('a').forEach(a => a.addEventListener('click', () => nav.classList.remove('open')));
+}
+
+// Reveal au scroll
+const io = new IntersectionObserver((entries) => {
+  entries.forEach(e => {
+    if (e.isIntersecting) { e.target.classList.add('in'); io.unobserve(e.target); }
+  });
+}, { threshold: 0.12 });
+document.querySelectorAll('.reveal').forEach((el, i) => {
+  el.style.transitionDelay = (i % 3) * 70 + 'ms';
+  io.observe(el);
+});
